ff76c5c55e636eb5124094bc3d4304b8df453352
savan-goti <vishals.webstore@gmail.com>
2025-12-25 22:05:49 +0530

    console error

--- resources/views/admin/layouts/footer-links.blade.php
+++ resources/views/admin/layouts/footer-links.blade.php
@@ -37,9 +37,16 @@
 
 <script src="{{asset('assets/admin/libs/sweetalert2/sweetalert2.min.js')}}"></script>
 
+@if(View::hasSection('particles') || request()->routeIs('admin.login') || request()->routeIs('admin.forgot-password') || request()->routeIs('admin.reset-password.form'))
 <script src="{{asset('assets/admin/libs/particles.js/particles.js')}}"></script>
 <!-- particles app js -->
-<script src="{{asset('assets/admin/js/pages/particles.app.js')}}"></script>
+<script>
+    // Only initialize particles if the container exists
+    if (document.getElementById('auth-particles')) {
+        particlesJS("auth-particles",{particles:{number:{value:90,density:{enable:!0,value_area:800}},color:{value:"#ffffff"},shape:{type:"circle",stroke:{width:0,color:"#000000"},polygon:{nb_sides:5},image:{src:"img/github.svg",width:100,height:100}},opacity:{value:.8,random:!0,anim:{enable:!0,speed:1,opacity_min:0,sync:!1}},size:{value:4,random:!0,anim:{enable:!1,speed:4,size_min:.2,sync:!1}},line_linked:{enable:!1,distance:150,color:"#ffffff",opacity:.4,width:1},move:{enable:!0,speed:2,direction:"none",random:!1,straight:!1,out_mode:"out",attract:{enable:!1,rotateX:600,rotateY:1200}}},interactivity:{detect_on:"canvas",events:{onhover:{enable:!0,mode:"bubble"},onclick:{enable:!0,mode:"repulse"},resize:!0},modes:{grab:{distance:400,line_linked:{opacity:1}},bubble:{distance:400,size:4,duration:2,opacity:.8,speed:3},repulse:{distance:200},push:{particles_nb:4},remove:{particles_nb:2}}},retina_detect:!0,config_demo:{hide_card:!1,background_color:"#b61924",background_image:"",background_position:"50% 50%",background_repeat:"no-repeat",background_size:"cover"}});
+    }
+</script>
+@endif
 <!-- password-addon init -->
 <script src="{{asset('assets/admin/js/pages/password-addon.init.js')}}"></script>
 
@@ -54,7 +61,7 @@
 <script src="{{asset('assets/admin/libs/datatables/js/pdfmake.min.js')}}"></script>
 <script src="{{asset('assets/admin/libs/datatables/js/jszip.min.js')}}"></script>
 
-<script src="{{asset('assets/admin/libs/%40ckeditor/ckeditor5-build-classic/build/ckeditor.js')}}"></script>
+<script src="{{ url('assets/admin/libs/@ckeditor/ckeditor5-build-classic/build/ckeditor.js') }}"></script>
 
 <!-- quill js -->
 {{--<script src="{{asset('assets/admin/libs/quill/quill.min.js')}}"></script>--}}
